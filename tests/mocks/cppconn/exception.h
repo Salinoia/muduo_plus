72b85f07297920de5c678ff40a3b345b61bbc734
Salinoia <dksbakvsh@gmail.com>
2025-09-14 16:14:42 +0800

    Modified

--- tests/mocks/cppconn/exception.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <exception>
-
-namespace sql {
-class SQLException : public std::exception {
-public:
-    const char* what() const noexcept override { return "sql exception"; }
-};
-}
